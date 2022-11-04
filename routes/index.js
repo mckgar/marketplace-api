13fd3923b1bbc33e30df8594259ae794f9b78c55
mckgar <kmgarner23@outlook.com>
2022-11-03 20:12:06 -0600

    Created router index

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const routes = (database) => {
+  const router = express.Router();
+  router.get('/', (req, res, next) => res.send('Under construction'));
+
+  //router.use('/account', require('./account')(database));
+
+  return router;
+};
+
+module.exports = routes;
