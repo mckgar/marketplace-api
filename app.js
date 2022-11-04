4e295f16dc46b1c06eb5d0ebf922f55b67cc4b1f
mckgar <kmgarner23@outlook.com>
2022-11-03 20:09:01 -0600

    Refactored to have db as dependency injection

--- app.js
+++ app.js
@@ -2,34 +2,35 @@ require('dotenv').config();
 const express = require('express');
 const logger = require('morgan');
 const createError = require('http-errors');
-const pool = require('./dbConfig');
-const routes = require('./routes/index');
 
-const app = express();
+const createApp = (database) => {
+  const app = express();
 
-pool.connect();
+  app.use(logger('dev'));
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+  const routes = require('./routes/index')(database);
+  app.use('/', routes);
 
-app.use('/', routes);
+  app.use((req, res, next) => {
+    next(createError(404));
+  });
 
-app.use((req, res, next) => {
-  next(createError(404));
-});
-
-app.use((err, req, res, next) => {
-  const message = req.app.get('env') === 'production' ? 'Oops, an error occured' : err.message;
-  if (req.app.get('env') !== 'production' && err.status !== 404) {
-    console.log(err);
-  }
-
-  return res.status(err.status || 500).json(
-    {
-      message
+  app.use((err, req, res, next) => {
+    const message = req.app.get('env') === 'production' ? 'Oops, an error occured' : err.message;
+    if (req.app.get('env') !== 'production' && err.status !== 404) {
+      console.log(err);
     }
-  )
-});
 
-module.exports = app;
+    return res.status(err.status || 500).json(
+      {
+        message
+      }
+    )
+  });
+
+  return app;
+}
+
+module.exports = createApp;
