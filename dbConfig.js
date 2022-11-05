fbdc9527dce9c3c1173d75e2e7f4e190a3401389
mckgar <kmgarner23@outlook.com>
2022-11-04 21:13:26 -0600

    Addded some database functions

--- dbConfig.js
+++ dbConfig.js
@@ -10,4 +10,38 @@ const pool = new Pool(
   }
 );
 
-module.exports = pool;
+exports.findAccountByUsername = async (username) => {
+  try {
+    const search = await pool.query(
+      'SELECT * FROM accounts WHERE username = $1 LIMIT 1',
+      [username]
+    );
+    return search.rows[0];
+  } catch (err) {
+    return err;
+  }
+};
+
+exports.findAccountByEmail = async (email) => {
+  try {
+    const search = await pool.query(
+      'SELECT * FROM accounts WHERE email = $1 LIMIT 1',
+      [email]
+    );
+    return search.rows[0];
+  } catch (err) {
+    return err;
+  }
+};
+
+exports.createAccount = async (username, hashedPassword, email) => {
+  try {
+    const newUser = await pool.query(
+      'INSERT INTO accounts(username, hashedPassword, email) VALUES($1, $2, $3)',
+      [username, hashedPassword, email]
+    );
+    return newUser.account_id;
+  } catch (err) {
+    return err;
+  }
+};
