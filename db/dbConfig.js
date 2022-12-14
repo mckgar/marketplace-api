7974a85fe76707bf67015dade1080894f4b37b7e
mckgar <kmgarner23@outlook.com>
2022-12-13 17:26:08 -0700

    Updated queries

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -211,7 +211,6 @@ exports.saveItem = async (sellerId, name, description, price, quantity, category
       `,
       [sellerId, name, description, price, quantity, category]
     );
-    return newItem.item_id;
   } catch (err) {
     return Promise.reject(err);
   }
@@ -223,15 +222,26 @@ exports.retrieveItems = async (price, category, offset, limit) => {
   try {
     const priceClause = price === 'relevent' ? '' :
       price === 'low' ? 'ORDER BY price' : 'ORDER BY price DESC';
-    const categoryClause = category === 'all' ? '' : `WHERE category = ${category}`;
+    const categoryClause = category === 'all' ?
+      ['books', 'clothing', 'toys', 'games', 'accessories', 'decorations', 'office'] :
+      [category];
     const search = await pool.query(
       `
-      SELECT * FROM items
-      ${categoryClause}
+      SELECT
+        item_id,
+        item_name AS name,
+        price,
+        quantity,
+        accounts.username AS seller,
+        categories.category_name AS category 
+      FROM items
+      LEFT JOIN accounts on items.seller = accounts.account_id
+      LEFT JOIN categories on items.category = categories.category_id
+      WHERE categories.category_name = ANY ($3)
       ${priceClause}
       LIMIT $2 OFFSET $1
       `,
-      [offset, limit]
+      [offset, limit, categoryClause]
     );
     return search.rows;
   } catch (err) {
@@ -243,7 +253,16 @@ exports.findItemsBySeller = async sellerId => {
   try {
     const search = await pool.query(
       `
-      SELECT * FROM items
+      SELECT 
+        item_id,
+        item_name AS name,
+        price,
+        quantity,
+        accounts.username AS seller,
+        categories.category_name AS category
+      FROM items
+      LEFT JOIN accounts on items.seller = accounts.account_id
+      LEFT JOIN categories on items.category = categories.category_id
       WHERE seller = $1
       `,
       [sellerId]
@@ -260,7 +279,18 @@ exports.findItemById = async (itemId) => {
   try {
     const search = await pool.query(
       `
-      SELECT * FROM items
+      SELECT 
+        item_id,
+        item_name AS name,
+        item_description AS description,
+        price,
+        quantity,
+        date_added,
+        accounts.username AS seller,
+        categories.category_name AS category
+      FROM items
+      LEFT JOIN accounts on items.seller = accounts.account_id
+      LEFT JOIN categories on items.category = categories.category_id
       WHERE item_id = $1
       LIMIT 1
       `,
@@ -390,8 +420,19 @@ exports.findCartByUser = async accountId => {
   try {
     const cart = await pool.query(
       `
-      SELECT * FROM carts
+      SELECT
+        items.item_id,
+        items.item_name AS name,
+        items.price,
+        cart_items.quantity,
+        accounts.username AS seller,
+        categories.category_name AS category,
+        cart_items.date_placed
+      FROM carts
       LEFT JOIN cart_items ON carts.cart_id = cart_items.cart
+      LEFT JOIN items ON cart_items.item = items.item_id
+      LEFT JOIN accounts ON items.seller = accounts.account_id
+      LEFT JOIN categories ON items.category = categories.category_id
       WHERE cart_owner = $1
       `,
       [accountId]
@@ -422,13 +463,107 @@ exports.addItemToCart = async (accountId, itemId, quantity) => {
       `,
       [itemId]
     );
-    const quantityToUse = Math.min(maxQuantity.rows[0].quantity, quantity)
+    const quantityToUse = Math.min(maxQuantity.rows[0].quantity, quantity);
     await pool.query(
       `
       INSERT INTO cart_items(cart, item, quantity, date_placed)
       VALUES($1, $2, $3, NOW())
       `,
-      [cartId, itemId, quantityToUse]
+      [cartId.rows[0].cart_id, itemId, quantityToUse]
+    );
+  } catch (err) {
+    return Promise.reject(err);
+  }
+}
+
+exports.removeItemFromCart = async (accountId, itemId, quantity) => {
+  try {
+    const cartId = await pool.query(
+      `
+      SELECT cart_id FROM carts
+      WHERE cart_owner = $1
+      LIMIT 1
+      `,
+      [accountId]
+    );
+    const maxQuantity = await pool.query(
+      `
+      SELECT quantity FROM cart_items
+      WHERE item = $1 AND cart = $2
+      LIMIT 1
+      `,
+      [itemId, cartId.rows[0].cart_id]
+    );
+    if (maxQuantity.rows[0].quantity > quantity) {
+      const quantityToUse = maxQuantity.rows[0].quantity - quantity;
+      await pool.query(
+        `
+        UPDATE cart_items
+        SET quantity = $3
+        WHERE cart = $1 AND item = $2
+        `,
+        [cartId.rows[0].cart_id, itemId, quantityToUse]
+      );
+    } else {
+      await pool.query(
+        `
+        DELETE FROM cart_items
+        WHERE cart = $1 AND item = $2
+        `,
+        [cartId.rows[0].cart_id, itemId]
+      );
+    }
+  } catch (err) {
+    return Promise.reject(err)
+  }
+}
+
+// Create order
+
+exports.submitOrder = async (email, cart) => {
+  try {
+    const order = await pool.query(
+      `
+      INSERT INTO orders(purchased_by, time_of_purchase)
+      VALUES ($1, NOW())
+      `,
+      [email]
+    );
+    for (const item of cart) {
+      const info = await pool.query(
+        `
+        SELECT price, quantity FROM items
+        WHERE item_id = $1
+        `,
+        [item.item_id]
+      );
+      const quantityToUse = Math.min(info.rows[0].quantity, item.quantity);
+      await pool.query(
+        `
+        INSERT INTO order_items(order_id, item_id, quantity, total)
+        VALUES ($1, $2, $3, $4)
+        `,
+        [order.rows[0].order_id, item.item_id, quantityToUse, quantityToUse * info.rows[0].price]
+      );
+      await pool.query(
+        `
+        UPDATE items
+        SET quantity = $1
+        WHERE item_id = $2
+        `,
+        [info.quantity - quantityToUse, item.item_id]
+      );
+    }
+    await pool.query(
+      `
+      UPDATE orders
+      SET amount_total = (
+        SELECT SUM(order_items.total) FROM order_items
+        WHERE order_items.order_id = $1
+      )
+      WHERE order_id = $1
+      `,
+      [order.rows[0].order_id]
     );
   } catch (err) {
     return Promise.reject(err);
